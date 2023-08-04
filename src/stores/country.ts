f3bf99857bc754fbf630416a0b55a086151b14c7
Fulberto TCHIAKPE <fulberto@MacBook-Pro-de-Fulberto.local>
2023-08-04 09:29:20 +0100

    fix: typescript issues fixed

--- src/stores/country.ts
+++ src/stores/country.ts
@@ -7,10 +7,10 @@ export const useCountryStore = defineStore('country', () => {
   const query = ref('')
 
   const country = ref(null)
-  const countries = ref([])
+  const countries = ref(<any>[])
 
   const region = ref(null)
-  const regions = ref([])
+  const regions = ref(<any>[])
 
   async function init() {
     const response = await getCountries()
@@ -18,10 +18,10 @@ export const useCountryStore = defineStore('country', () => {
     const collection = collect(response)
     const sorted = collection.sortBy('name.common')
     countries.value = sorted.all()
-    regions.value = [...new Set(countries.value.map((item) => item.region))]
+    regions.value = [...new Set(countries.value.map((item: any) => item?.region))]
   }
 
-  function setQuery(q) {
+  function setQuery(q: any) {
     query.value = q
   }
 
@@ -38,7 +38,7 @@ export const useCountryStore = defineStore('country', () => {
       baseURL: 'https://restcountries.com/v3.1/'
     })
 
-    const interceptor = axios.interceptors.response.use(
+    instance.interceptors.response.use(
       function (response) {
         return response
       },
@@ -47,9 +47,9 @@ export const useCountryStore = defineStore('country', () => {
         return Promise.resolve([])
       }
     )
-
-    instance.interceptors.response.use(interceptor)
-    const response = await instance.get('all?fields=name,flags,population,capital,region,languages,tld,currencies')
+    const response = await instance.get(
+      'all?fields=name,flags,population,capital,region,languages,tld,currencies'
+    )
     return response.data
   }
 
